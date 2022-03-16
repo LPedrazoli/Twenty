3b4d68ed1f8318bed2e2b6abf8d1cd7cd47c7425
Pedrazoli <lawpedrazoli@FVFZMYC2J1WK.tld>
2022-03-16 13:58:16 -0500

    Class1

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
-computerScore = 0;
-userScore = 0;
-computerNumber = 0;
-userNumber = 0;
+do { 
+    computerScore = 0;
+    userScore = 0;
+    computerNumber = 0;
+    userNumber = 0;
 
-do {
-    var numbers = Math.floor(Math.random() * 10) +1;
-    computerNumber = computerNumber + numbers;
-    alert(`The Computer got ${numbers}. The Computers new total is ${computerNumber}`)
-} while (computerNumber <= 16)
+    do {
+        var numbers = Math.floor(Math.random() * 10) +1;
+        computerNumber = computerNumber + numbers;
+        alert(`The Computer got ${numbers}. The Computers new total is ${computerNumber}`)
+    } while (computerNumber <= 16)
 
-alert("It is now your turn !")
+    alert("It is now your turn!!!")
 
-do {
-    var numbers = Math.floor(Math.random() * 10) +1;
-    userNumber = userNumber + numbers;
-    alert(`The number you got was ${numbers}. Your new total is ${computerNumber}`)
-    var exit = prompt(`Would you like to exit? If so, type "Yes".`)
+    do {
+        var numbers = Math.floor(Math.random() * 10) +1;
+        userNumber = userNumber + numbers;
+        alert(`The number you got was ${numbers}. Your new total is ${userNumber}`)
+        var play = prompt(`Would you like to go again? If so, type "Yes".`)
+    } while ((userNumber <= 19) && (play == "Yes"))
 
-} while ((userNumber <= 19) && (exit != "Yes"))
+    alert(`Computer: ${computerNumber}. User: ${userNumber}.`)
 
-alert(`Computer: ${computerNumber}. User: ${userNumber}.`)
+    if (computerNumber > userNumber) {
+        alert(`The computer has won with a score of ${computerNumber}.`)
+        computerScore++
+        alert(`The score is; Computer: ${computerScore} Human: ${userScore}`)
+    } else if (userNumber > computerNumber) {
+        alert(`Congratulations! You won with a score of ${userNumber}`)
+        userScore++
+        alert(`The score is; Computer: ${computerScore} Human: ${userScore}`)
+    } else {
+        alert(`You drew with the bot no points for anyone!`)
+        alert(`The score is; Computer: ${computerScore} Human: ${userScore}`)
+    }
 
-if (computerNumber > userNumber) {
-    alert(`The computer has won with a score of ${computerNumber}.`)
-    computerScore++
-} else if (userNumber > computerNumber) {
-    alert(`Congratulations! You won with a score of ${userNumber}`)
-    userScore++
-} else {
-    alert(`You drew with the bot no points for anyone!`)
-}
-alert(`The score is; Computer: ${computerScore} Human: ${userScore}`)
+} while exit = prompt(`Would you like to exit? If so, type "Yes".`)
